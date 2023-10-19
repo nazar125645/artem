09e2faea632a3422de53a3e2cfcb837b70d5dff7
nazar125645 <98477696+nazar125645@users.noreply.github.com>
2023-10-20 00:55:23 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "21.02.2004"; // Дата рождения
+var fullname = "Семиділко Артем Евгенійович" // ФИО
+var name = "Артем" // Имя
+
+// Паспорт
+var passport_id = "006929576" // Номер паспорта
+
+// КПП
+var kpp_id = "3931903097" // ИНН
+
+// Права
+var rights_valid_until = "11.9.2025" // Права "Дійсні до"
+var rights_categories = "A, A1, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "27.12.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
